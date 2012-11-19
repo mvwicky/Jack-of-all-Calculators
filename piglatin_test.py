6d84c2bf654623f3e88fe6a9431420daccbca294
Michael Van Wickle <mvanwickle@gmail.com>
2012-11-19 06:41:55 -0800

    piglatin

---
+++ piglatin_test.py
@@ -0,0 +1,18 @@
+import os
+
+word = raw_input("Word = ")
+print word
+end = ""
+end = word[0]
+if word[0] == "a":
+	newword = word + "way"
+if word[0] == "e":
+	newword = word + "way"
+if word[0] == "i":
+	newword = word + "way"
+if word[0] == "o":
+	newword = word + "way"
+if word[0] == "u":
+	newword = word + "way"	
+print newword
+os.system('pause')
