0b8c31650d1bdb75393fa26bfe31943faf16f8c7
Michael Van Wickle <mvanwickle@gmail.com>
2012-11-19 07:55:40 -0800

    Update piglatin_test.py

--- piglatin_test.py
+++ piglatin_test.py
@@ -1,6 +1,7 @@
 import os
 
 word = raw_input("Word = ")
+str(word)
 print word
 end = ""
 end = word[0]
@@ -13,6 +14,8 @@ if word[0] == "i":
 if word[0] == "o":
 	newword = word + "way"
 if word[0] == "u":
-	newword = word + "way"	
+	newword = word + "way"
+else:
+        newword = word[1 : len(word)] + "ay"
 print newword
 os.system('pause')
