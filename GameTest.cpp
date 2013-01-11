e69d80285c953f6479da6a370a51782d5aeb907a
Michael Van Wickle <mvanwickle@gmail.com>
2013-01-10 18:24:40 -0800

    update GameTest.cpp

--- GameTest.cpp
+++ GameTest.cpp
@@ -24,12 +24,31 @@ struct character {
 
 int main()
 {
+	srand(time(NULL));
+	setupMe();
+	//int to_me;
+	int from_me;
 	bool in = true;
+	string w;
 	while (in == true)
 	{
-		string fward;
 		cout << "w" << endl;
-		cin >> fward;
+		cin >> w;
+		setupEnemy();
+
+		cout << "watch out!, an enemy!";
+		cout << "Enemy has " << enemy[enemies].health << "health" << endl; 
+		cin.ignore();
+		while (enemy[enemies].health >= 0)
+		{
+			from_me = strike(me.strength) - defend(enemy[enemies].defence);
+			enemy[enemies].health = enemy[enemies].health - from_me; 
+			cout << "You deal " << from_me << " damage" << endl;
+			cout << "Enemy has " << enemy[enemies].health << "health left" << endl;
+			cin.ignore();
+			if (enemy[enemies].health == 0)
+				break;
+		}
 		in = stay();
 	}
 	return 0;
@@ -39,7 +58,6 @@ int main()
 int strike(int slevel)
 {
 	int hit;
-	srand(time(NULL));
 	hit = (rand() % (slevel + 1));
 	return hit;
 }
@@ -47,7 +65,6 @@ int strike(int slevel)
 int defend(int dlevel)
 {
 	int def;
-	srand(time(NULL));
 	def = (rand() % (dlevel + 1));
 	return def;
 }
@@ -56,11 +73,10 @@ int gEvent()
 {
 	bool ev;
 	int rnum;
-	srand(time(NULL));
 	rnum = (rand() % 2);
 	if (rnum == 1)
 		ev = true;
-	else
+	if (rnum == 0)
 		ev = false;
 	return ev;
 }
@@ -70,19 +86,25 @@ void setupMe()
 	me.level = 1;
 	me.expe = 0;
 	me.health = 15;
-	me.strength = 1;
-	me.defence = 1;
+	me.strength = 3;
+	me.defence = 3;
 }
 
 void setupEnemy()
 {
-	srand(time(NULL));
+	enemies += 1;
 	enemy[enemies].level = (rand() % me.level);
 	enemy[enemies].expe = 0;
-	enemy[enemies].health = (rand() % me.health);
+	enemy[enemies].health = 15;
 	enemy[enemies].strength = (rand() % me.level);
 	enemy[enemies].defence = (rand() % me.defence);
-	enemies += 1;
+	cout << enemy[enemies].level << endl;
+	cout << enemy[enemies].expe << endl;
+	cout << enemy[enemies].health << endl;
+	cout << enemy[enemies].strength << endl;
+	cout << enemy[enemies].defence << endl;
+
+	
 }
 
 bool stay()
